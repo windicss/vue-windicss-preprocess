1d6c680e767fbe2f13efa3200f17371999ce58ab
voorjaar <felitas@zoho.com>
2021-01-03 23:34:41 +0800

    added nuxt example

---
+++ example/nuxt/nuxt.config.js
@@ -0,0 +1,49 @@
+export default {
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'nuxt_example',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+  ],
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    extend(config) {
+      config.module.rules.push({
+        test: /\.vue$/,
+        loader: '../../src/index.js',
+        options: {
+          compile: true, // false: interpretation mode; true: compilation mode
+          globalPreflight: true,  // preflight style is global or scoped
+          globalUtility: true,  // utility style is global or scoped
+          // prefix: 'windi-',
+        }
+      })
+    }
+  }
+}
---
+++ example/nuxt/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <main>
+    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12 test">
+      <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div class="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <div class="max-w-md mx-auto">
+            <div>
+              <Logo/>
+            </div>
+            <div class="divide-y divide-gray-300">
+              <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                <p>An advanced online playground for Tailwind CSS, including support for things like:</p>
+                <ul class="list-disc space-y-2">
+                  <Feature>
+                    Customizing your
+                      <code class="text-sm font-bold text-gray-900">tailwind.config.js</code> file
+                  </Feature>
+                  <Feature>
+                    Extracting classes with
+                      <code class="text-sm font-bold text-gray-900">@apply</code>
+                  </Feature>
+                  <Feature>
+                    Code completion with instant preview
+                  </Feature>
+                </ul>
+                <p>Perfect for learning how the framework works, prototyping a new idea, or creating a demo to share online.</p>
+              </div>
+              <div class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7">
+                <p>Want to dig deeper into Tailwind?</p>
+                <p>
+                  <a href="https://tailwindcss.com/docs" class="text-cyan-600 hover:text-cyan-700"> Read the docs &rarr; </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
