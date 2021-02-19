d16c569fc876ed497a57617462acc836f703f5de
reslear <reslear@gmail.com>
2021-02-19 19:42:23 +0300

    feat: nuxt composition api & typescript example

---
+++ example/nuxt-composition/pages/nested.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="a">
+      <div class="b">Text</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+export default Vue.extend({
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {},
+})
+</script>
+
+<style lang="postcss" scoped>
+.a {
+  & .b {
+    @apply text-black;
+
+    &:hover {
+      @apply text-green;
+    }
+  }
+}
+</style>
