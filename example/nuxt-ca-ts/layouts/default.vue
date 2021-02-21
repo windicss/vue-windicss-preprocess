d04280a4d8a6d128285f4f1105ecfcc902ab78f1
reslear <reslear@gmail.com>
2021-02-21 20:13:18 +0300

    refactor(examples): commented not working lines

--- example/nuxt-ca-ts/layouts/default.vue
+++ example/nuxt-ca-ts/layouts/default.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="container m-auto max-w-xl min-h-screen py-10"><nuxt></nuxt></div>
+  <div class="container max-w-xl min-h-screen py-10 m-auto">
+    <Nuxt />
+  </div>
 </template>
